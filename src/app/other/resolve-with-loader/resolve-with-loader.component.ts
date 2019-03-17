b76e388f2e1b2f1c229a2a30a0b5fc7fb118d01c
henrikmathiesen <henrik_mathiesen@hotmail.com>
2019-03-17 13:03:06 +0100

    -loader -navigate between step 1 and 2

--- src/app/other/resolve-with-loader/resolve-with-loader.component.ts
+++ src/app/other/resolve-with-loader/resolve-with-loader.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
     templateUrl: './resolve-with-loader.component.html'
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
 export class ResolveWithLoaderComponent {
 
     pageTitle = 'Resolve with Loader';
+
+    constructor(
+        private router: Router,
+        private activatedRoute: ActivatedRoute) { }
+
+    onPrev() { 
+        this.router.navigate(['step1'], { relativeTo: this.activatedRoute });
+    }
+
+    onNext() { 
+        this.router.navigate(['step2'], { relativeTo: this.activatedRoute });
+    }
 
 }
