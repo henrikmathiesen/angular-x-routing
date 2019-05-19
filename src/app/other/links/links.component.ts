935f96414c9a9f50540070c126e369f8c6dc049d
henrikmathiesen <henrik_mathiesen@hotmail.com>
2019-05-19 12:36:29 +0200

    -router navigation repetition

--- src/app/other/links/links.component.ts
+++ src/app/other/links/links.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     templateUrl: './links.component.html'
@@ -6,8 +7,25 @@ import { Component } from '@angular/core';
 export class LinksComponent {
 
     pageTitle = 'Links';
+    productId = 2;
 
     // TODO: Link from here to products and from products to here. From code and from template.
     // And to other/resolve-with-loader
 
+
+    constructor(
+        private router: Router
+    ) { }
+
+    onGoToOtherOne() {
+        this.router.navigateByUrl('/other/tooltip');
+    }
+
+    onGoToOtherTwo() { 
+        this.router.navigate(['/other', 'tooltip']);
+    }
+
+    onGotoProductWithId() {
+        this.router.navigate(['/products', this.productId, 'edit']);
+    }
 }
