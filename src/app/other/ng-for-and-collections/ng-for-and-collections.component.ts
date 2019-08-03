e8a3d913e3a3a2fcd8b345361de4ea08108bfc4b
henrikmathiesen <henrik_mathiesen@hotmail.com>
2019-08-03 13:14:22 +0200

    -nested ngFor works

--- src/app/other/ng-for-and-collections/ng-for-and-collections.component.ts
+++ src/app/other/ng-for-and-collections/ng-for-and-collections.component.ts
@@ -1,19 +1,46 @@
 import { Component } from '@angular/core';
 
+interface Person {
+    name: string;
+    age: number;
+}
+
+interface PersonWithFriends extends Person {
+    friends: Person[]
+}
+
 @Component({
     templateUrl: './ng-for-and-collections.component.html'
 })
 export class NgForAndCollectionsComponent {
     pageTitle = 'ngFor and collections';
 
-    someCollection = [
+    people: PersonWithFriends[] = [
         {
             name: 'Adam',
             age: 30,
             friends: [
                 {
-                    name: 'Adams',
-                    age: 25
+                    name: 'Adams 1',
+                    age: 20
+                },
+                {
+                    name: 'Adams 2',
+                    age: 21
+                }
+            ]
+        },
+        {
+            name: 'Bertil',
+            age: 40,
+            friends: [
+                {
+                    name: 'Bertils 1',
+                    age: 18
+                },
+                {
+                    name: 'Bertils 2',
+                    age: 19
                 }
             ]
         }
