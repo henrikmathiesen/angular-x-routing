d5a796b851c85c7307196b9083a72c0d54a312e6
henrikmathiesen <henrik_mathiesen@hotmail.com>
2019-08-06 18:43:35 +0200

    -rating

--- src/app/other/rating-bootstrap/rating-bootstrap.component.ts
+++ src/app/other/rating-bootstrap/rating-bootstrap.component.ts
@@ -1,8 +1,42 @@
 import { Component } from '@angular/core';
 
 @Component({
-    templateUrl: './rating-bootstrap.component.html'
+    templateUrl: './rating-bootstrap.component.html',
+    styles: [`
+    .star {
+      position: relative;
+      display: inline-block;
+      font-size: 3rem;
+      color: #d3d3d3;
+    }
+    .full {
+      color: red;
+    }
+    .half {
+      position: absolute;
+      display: inline-block;
+      overflow: hidden;
+      color: red;
+    }
+  `]
 })
 export class RatingBootstrapComponent {
     pageTitle = 'Rating Bootstrap';
+
+    basicCurrentRate = 5;
+
+    heartsCurrentRate = 0;
+    heartsRating = 0;
+
+    setHeartsRating() {
+        if (!this.heartsRating || isNaN(this.heartsRating)) {
+            this.heartsCurrentRate = 0;
+            return;
+        }
+
+        let heartsCurrentRate = this.heartsRating < 0 ? 0 : this.heartsRating;
+        heartsCurrentRate = heartsCurrentRate > 5 ? 5 : heartsCurrentRate;
+
+        this.heartsCurrentRate = heartsCurrentRate;
+    }
 }
